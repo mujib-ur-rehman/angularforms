f39dbf1924b51929d987d610aedc697b1514f0a7
mujib-ur-rehman <mujib.rehman@focusteck.com>
2021-10-18 18:28:46 +0500

    passing data from parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  user = {
+    name: '',
+    age: '',
+    number: ''
+  };
   title = 'fromsangular';
+  getValues(simpleForm: any) {
+    this.user.name = simpleForm.user;
+    this.user.age = simpleForm.age;
+    this.user.number = simpleForm.contact;
+    // console.log(this.user.name);
+    // alert(this.user.name);
+  }
+  // outputValues() {
+  //   alert(this.user.name);
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ChildComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
